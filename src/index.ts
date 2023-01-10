41c5fb7b0f393d0d80fe3ad6ddd5da4eba8116b7
MohamedTarekBar <77550770+MohamedTarekBar@users.noreply.github.com>
2023-01-10 03:24:53 +0200

    postman doc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -13,12 +13,15 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
 app.use('/api', api);
-app.use(()=>{
-    throw sendErr(Side.page404,constants.default.pageNotFound);
+app.get('/', (_req: Request, res: Response) => {
+    res.redirect('https://documenter.getpostman.com/view/13127908/2s8Z76uUSG');
+});
+app.use(() => {
+    throw sendErr(Side.page404, constants.default.pageNotFound);
 });
 app.use(errorMiddlewere);
 app.listen(PORT, () => {
-    const server = '::::3000';
+    const server = 'localhost:3000';
     console.log('app is running on' + server);
 });
 export default app;
